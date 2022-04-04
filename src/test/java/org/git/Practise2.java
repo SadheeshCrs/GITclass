82bdff9c6cb80195eecea856565f24aa0dcf4a7e
SadheeshCrs <sadheeshppco23@gmail.com>
2022-04-03 23:36:43 -1100

    Added Practise2

---
+++ src/test/java/org/git/Practise2.java
@@ -0,0 +1,19 @@
+package org.git;
+
+public class Practise2 {
+	
+	public void method1() {
+		System.out.println("method 1");
+	}
+	
+	public void method2() {
+		System.out.println("method 1");
+
+	}
+	
+	public void method3() {
+		System.out.println("method 1");
+
+	}
+
+}
